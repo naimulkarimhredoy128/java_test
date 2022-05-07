85d99c09913c2489b585dc788083227e1fabc7c7
naimul karim <naimulkarim8965@gmail.com>
2022-05-08 00:28:18 +0600

    initial commit

---
+++ src/myfancyjavaproject/Painter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfancyjavaproject;
+
+/**
+ *
+ * @author HP
+ */
+public class Painter {
+    public static void main(String[] args) {
+        Circle p=new Circle();
+        p.draw();
+    }
+}
